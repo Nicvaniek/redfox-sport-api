605250883bf67b88f50768f42471db66814cf184
Nicolai van Niekerk <nicvaniek@gmail.com>
2020-03-01 19:54:09 +0200

    Initial setup

---
+++ redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/RedFoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.redfox.redfoxsportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedFoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedFoxApiApplication.class, args);
+	}
+
+}
