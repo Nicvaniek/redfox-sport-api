1dd03afd6e6889af7a357156018f0e50817514c1
Nicolai van Niekerk <nicvaniek@gmail.com>
2020-05-03 18:50:41 +0200

    Add endpoint to retrieve sport headlines

--- redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/RedFoxApiApplication.java
+++ redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/RedFoxApiApplication.java
@@ -2,8 +2,10 @@ package com.redfox.redfoxsportapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RedFoxApiApplication {
 
 	public static void main(String[] args) {
---
+++ redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/models/news/Source.java
@@ -0,0 +1,11 @@
+package com.redfox.redfoxsportapi.models.news;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Source {
+    private String id;
+    private String name;
+}
