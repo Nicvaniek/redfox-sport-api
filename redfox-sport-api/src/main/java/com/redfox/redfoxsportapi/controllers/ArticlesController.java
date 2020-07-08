933d6692c3b9077369cc67e4d159422e4401b385
Nicolai van Niekerk <nicvaniek@gmail.com>
2020-07-08 18:22:01 +0200

    Add springfox swagger config

--- redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/controllers/ArticlesController.java
+++ redfox-sport-api/src/main/java/com/redfox/redfoxsportapi/controllers/ArticlesController.java
@@ -3,6 +3,7 @@ package com.redfox.redfoxsportapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import com.redfox.redfoxsportapi.services.articles.ArticlesService;
 
 @RestController
 @RequestMapping(path = "articles")
+@CrossOrigin
 public class ArticlesController {
     
     private ArticlesService articlesService;
